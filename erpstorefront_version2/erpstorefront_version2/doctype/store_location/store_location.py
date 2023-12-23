0e17f707ffbf4a8e8ba4d76232746719a37d323f
MarkSuttirak <116248336+MarkSuttirak@users.noreply.github.com>
2023-12-23 15:12:18 +0700

    Upload doctypes of erpstorefront_version2

---
+++ erpstorefront_version2/erpstorefront_version2/doctype/store_location/store_location.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, mark and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class StoreLocation(Document):
+	pass
